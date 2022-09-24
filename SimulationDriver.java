bf760e5f49ab64d7293b1d40cef6914cb0801da2
LaurenceTimothyMGarcia <LaurenceTimothyMGarcia@users.noreply.github.com>
2022-09-24 15:32:31 -0700

    declared questions and students to test

--- SimulationDriver.java
+++ SimulationDriver.java
@@ -4,13 +4,48 @@
 //Professor Yu Sun
 
 import java.util.*;
+import java.io.*;
 
 public class SimulationDriver
 {
     public static void main(String args[])
     {
-        
+        Scanner kb = new Scanner(System.in);
+
+        System.out.print("How many students are there? ");
+        int numOfStudents = kb.nextInt();
+
+        //List of questions
+        Question q1 = new QuestionMC("What is an interface?");
+        Question q2 = new QuestionTF("Overwatch was created by Riot Games.");
+        Question q3 = new QuestionMC("Who created Stardew Valley?");
+        Question q4 = new QuestionMC("Who is the main character of the Amazing World of Gumball?");
+        Question q5 = new QuestionTF("Apple creates Samsung Phones");
+        Question q6 = new QuestionTF("Batman is based off of a bat.");
+
+        //Students
+        Student s1 = new Student();
+        Student s2 = new Student();
+        Student s3 = new Student();
+        Student s4 = new Student();
+        Student s5 = new Student();
+        Student s6 = new Student();
+        Student s7 = new Student();
+        Student s8 = new Student();
+        Student s9 = new Student();
+        Student s10 = new Student();
+        Student s11 = new Student();
+        Student s12 = new Student();
+        Student s13 = new Student();
+        Student s14 = new Student();
+        Student s15 = new Student();
+        Student s16 = new Student();
+        Student s17 = new Student();
+        Student s18 = new Student();
+        Student s19 = new Student();
+        Student s20 = new Student();
 
         
+
     }
 }
