687f76cd6edebe7fe123c5b3ed4cdc432b9a3a48
LaurenceTimothyMGarcia <LaurenceTimothyMGarcia@users.noreply.github.com>
2022-09-24 08:05:59 -0700

    Updated question.java to just have constants

--- Question.java
+++ Question.java
@@ -1,54 +1,8 @@
 public interface Question
 {
-//No need for answer
-
     //MC questions
-    private char[] multiChoice;
-    private int mcAnswer;
+    public final String[] multiChoice = {"A", "B", "C", "D", "E", "F"};
 
     //True false questions
-    private boolean trueFalse;
-    private boolean trueFalseAnswer;
-
-    public Question(boolean tF, boolean tfAnswer)
-    {
-        trueFalse = tF;
-        trueFalseAnswer = tfAnswer;
-    }
-
-    public Question(char[] mc, int answer)
-    {
-        multiChoice = mc;
-        mcAnswer = answer;
-    }
-
-    public char[] getMCQuestion()
-    {
-        return multiChoice;
-    }
-
-    public int getMCAnswer()
-    {
-        return mcAnswer;
-    }
-
-    public boolean getTrueFalse()
-    {
-        return trueFalse;
-    }
-
-    public boolean getTFAnswer()
-    {
-        return trueFalseAnswer;
-    }
-
-    public void setAnswer(int mcAns)
-    {
-        mcAnswer = mcAns;
-    }
-
-    public void setAnswer(boolean tfAns)
-    {
-        trueFalseAnswer = tfAns;
-    }
+    public final String[] trueFalse = {"True", "False"};
 }
