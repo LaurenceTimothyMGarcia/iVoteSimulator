c93e8826c4fdb0396a5adddf52ed469f7cccc02b
LaurenceTimothyMGarcia <LaurenceTimothyMGarcia@users.noreply.github.com>
2022-09-24 12:25:29 -0700

    added new files for true false and mc specifically

---
+++ QuestionTF.java
@@ -0,0 +1,20 @@
+public class QuestionTF implements Question
+{
+    //Prints the question for display for the user
+    public void PrintQuestion()
+    {
+
+    }
+
+    //Displays the choices for the user
+    public void PrintChoice()
+    {
+
+    }
+
+    //Counts how many times a student selected that number
+    public void AddCounter()
+    {
+
+    }
+}
