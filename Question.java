45980c5d3af07142bfd969f5e0993000cf0e96da
LaurenceTimothyMGarcia <LaurenceTimothyMGarcia@users.noreply.github.com>
2022-09-24 08:16:29 -0700

    Working on classes for student and question

--- Question.java
+++ Question.java
@@ -4,5 +4,7 @@ public interface Question
     public final String[] multiChoice = {"A", "B", "C", "D", "E", "F"};
 
     //True false questions
-    public final String[] trueFalse = {"True", "False"};
+    public final String[] trueFalse = {"1. True", "2. False"};
+
+    public void printChoice();
 }
--- Student.java
+++ Student.java
@@ -1,4 +1,21 @@
 public class Student
 {
-    
+    private String studentID;
+
+    private String mcChoice;
+    private int tfChoice;
+
+    public void Student(String id, String mc)
+    {
+        studentID = id;
+        mcChoice = mc;
+    }
+
+    public void Student(String id, int tf)
+    {
+        studentID = id;
+        tfChoice = tf;
+    }
+
+
 }
